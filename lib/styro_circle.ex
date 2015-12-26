e0b7d585a2a32eca2a6ebdf12cf56bcc3327a87a
Michael A Anderson <usufruct@gmail.com>
2015-12-26 12:22:03 -0600

    graph is something that can be updated

--- lib/styro_circle.ex
+++ lib/styro_circle.ex
@@ -1,2 +1,27 @@
 defmodule StyroCircle do
+  def new_graph do
+    Agent.start_link(fn ->
+      :digraph.new()
+    end)
+  end
+
+  def digraph(graph) do
+    Agent.get(graph, fn digraph ->
+      digraph
+    end)
+  end
+
+  def add_vertex(graph, vertex, label) do
+    Agent.update(graph, fn digraph ->
+      :digraph.add_vertex(digraph, vertex, label)
+      digraph
+    end)
+  end
+
+  def add_edge(graph, vertex1, vertex2, label) do
+    Agent.update(graph, fn digraph ->
+      :digraph.add_edge(digraph, vertex1, vertex2, label)
+      digraph
+    end)
+  end
 end
--- test/styro_circle_test.exs
+++ test/styro_circle_test.exs
@@ -2,7 +2,31 @@ defmodule StyroCircleTest do
   use ExUnit.Case
   doctest StyroCircle
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "new_graph" do
+    {:ok, graph} = StyroCircle.new_graph
+    assert is_pid graph
+  end
+
+  test "digraph" do
+    {:ok, graph} = StyroCircle.new_graph
+    assert StyroCircle.digraph(graph)
+           |> :digraph.no_edges == 0
+  end
+
+  test "add_vertex" do
+    {:ok, graph} = StyroCircle.new_graph
+    StyroCircle.add_vertex(graph, :foo, {})
+    StyroCircle.add_vertex(graph, :bar, {})
+    assert StyroCircle.digraph(graph)
+           |> :digraph.no_vertices == 2
+  end
+
+  test "add_edge" do
+    {:ok, graph} = StyroCircle.new_graph
+    StyroCircle.add_vertex(graph, :foo, {})
+    StyroCircle.add_vertex(graph, :bar, {})
+    StyroCircle.add_edge(graph, :foo, :bar, {})
+    assert StyroCircle.digraph(graph)
+           |> :digraph.no_edges == 1
   end
 end
